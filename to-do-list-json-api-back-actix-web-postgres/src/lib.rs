a57c894248ffb059c5580142b1486f7fb40bd6db
Oliver Forral <intrepion@gmail.com>
2022-02-28 16:56:59 -0700

    green

--- to-do-list-json-api-back-actix-web-postgres/src/lib.rs
+++ to-do-list-json-api-back-actix-web-postgres/src/lib.rs
@@ -1,12 +1,17 @@
 #[cfg(test)]
 mod tests {
+    use super::index;
+    use actix_web::{test, App};
+
     #[actix_web::test]
     async fn it_works() {
-        let mut app = test::init_service(
-            App::new()
-                .wrap(middleware::Logger::default())
-                .service(index),
-        )
-        .await;
+        let mut _app = test::init_service(App::new().service(index)).await;
     }
+}
+
+use actix_web::{get, Error, HttpResponse};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().body("Hello world!"))
 }
