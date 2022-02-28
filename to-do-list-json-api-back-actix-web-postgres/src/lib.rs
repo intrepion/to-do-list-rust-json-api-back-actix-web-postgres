4650726b3f089bd5f38b3f2d1dba33041a297433
Oliver Forral <intrepion@gmail.com>
2022-02-28 16:53:13 -0700

    red

--- to-do-list-json-api-back-actix-web-postgres/src/lib.rs
+++ to-do-list-json-api-back-actix-web-postgres/src/lib.rs
@@ -1,9 +1,12 @@
 #[cfg(test)]
 mod tests {
-
     #[actix_web::test]
     async fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut app = test::init_service(
+            App::new()
+                .wrap(middleware::Logger::default())
+                .service(index),
+        )
+        .await;
     }
 }
