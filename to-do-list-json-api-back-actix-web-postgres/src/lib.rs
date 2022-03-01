aa7159ce6a8fef188eb79a8ad915d1dc9a43669b
Oliver Forral <intrepion@gmail.com>
2022-02-28 17:04:11 -0700

    blue

--- to-do-list-json-api-back-actix-web-postgres/src/lib.rs
+++ to-do-list-json-api-back-actix-web-postgres/src/lib.rs
@@ -5,11 +5,13 @@ mod tests {
     use actix_web::{test, App};
 
     #[actix_web::test]
-    async fn it_works() {
-        let mut _app = test::init_service(App::new().service(index)).await;
-        let _req = test::TestRequest::default()
+    async fn test_index_get() {
+        let app = test::init_service(App::new().service(index)).await;
+        let req = test::TestRequest::default()
             .insert_header(ContentType::plaintext())
             .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
     }
 }
 
